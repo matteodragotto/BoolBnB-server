2e4fa299ee2796ddb09d7f18e4aabbeae0d697bf
l-martinetti <luca.martinetti@outlook.it>
2025-02-20 12:55:49 +0100

    FEAT: add destroy route

--- controllers/b&bController.js
+++ controllers/b&bController.js
@@ -38,8 +38,16 @@ const modify = (req, res) => {
 }
 
 const destroy = (req, res) => {
-  res.send('Cancello un immobile')
-}
+  const id = req.params.id;
+  const sql = 'DELETE FROM real_estate WHERE id = ?';
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: 'Query al database fallita' })
+    if (results.length === 0) return res.status(404).json({ error: "Record not found" });
+
+    res.sendStatus(204)
+  })
+};
 
 module.exports = {
   index,
