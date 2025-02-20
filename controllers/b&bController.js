ebb9b40c91ebb8e92bffdbad9ddff56a3d67e340
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-02-20 16:14:06 +0100

    [FEAT] add show route

--- controllers/b&bController.js
+++ controllers/b&bController.js
@@ -1,4 +1,5 @@
 const connection = require('../data/db')
+const path = require('path')
 
 
 const index = (req, res) => {
@@ -22,7 +23,41 @@ const index = (req, res) => {
 }
 
 const show = (req, res) => {
-  res.send('Mostra il dettaglio immobile')
+  const id = req.params.id;
+
+  const sql = `SELECT I.*, ROUND(AVG(R.voto)) AS media_voti
+  FROM real_estate I
+  LEFT JOIN reviews R ON I.id = R.real_estate_id
+  WHERE I.id = ?
+  GROUP BY I.id`
+
+  const sqlImage = `SELECT images.url FROM images WHERE real_estate_id = ?`
+
+  const sqlReviews = `SELECT * FROM reviews WHERE real_estate_id = ?`
+
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: err.message })
+    if (results.length === 0) return res.status(404).json({ error: 'Immobile non trovato' })
+
+    const real_estate = results[0];
+
+    connection.query(sqlImage, [id], (err, resultsImg) => {
+      if (err) return res.status(500).json({ error: err.message })
+
+      const correctedImage = resultsImg;
+
+
+      connection.query(sqlReviews, [id], (err, resultsReviews) => {
+        if (err) return res.status(500).json({ error: err.message })
+        real_estate.reviews = resultsReviews.length > 0 ? resultsReviews : [];
+        res.json({
+          ...real_estate,
+          image_urls: correctedImage
+        })
+      })
+    })
+
+  })
 }
 
 const store = (req, res) => {
