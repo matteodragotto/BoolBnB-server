8bfc84837d79e845a8eadb60942624d8049bc779
galdoben <galdobenedetta@gmail.com>
2025-02-20 16:19:17 +0100

    feat: store-route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const port = process.env.port
+const port = process.env.port || 3002
 const bnbRouter = require('./router/b&b')
 const errorsHandler = require('./middlewares/errorsHandler')
 const notFound = require('./middlewares/notFound')
