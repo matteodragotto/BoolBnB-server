e1d36fe457ca52a55c290f53c4b7c642fb3d0459
matteodragotto <matteodrag8@gmail.com>
2025-02-20 12:01:30 +0100

    [feat] database mysql

---
+++ data/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
+})
+
+connection.connect((err) => {
+  if (err) throw err
+
+  console.log('Connesso a mySQL');
+})
+
+module.exports = connection
