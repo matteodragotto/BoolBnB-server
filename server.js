9ac7c722642aeb7b1c13bf3c1148df4cac4a5252
matteodragotto <matteodrag8@gmail.com>
2025-02-20 11:59:53 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const port = process.env.port
+const bnbRouter = require('./router/b&b')
+const errorsHandler = require('./middlewares/errorsHandler')
+const notFound = require('./middlewares/notFound')
+const setImagePath = require('./middlewares/imagePath')
+
+const app = express()
+
+app.use(cors({ origin: 'http://localhost:5173' }))
+
+app.use(express.static('public'))
+
+app.use(express.json())
+
+app.use(setImagePath)
+
+app.get('/', (req, res) => {
+  res.send('Server di BoolB&B')
+})
+
+app.use('/immobili', bnbRouter)
+
+app.use(errorsHandler)
+
+app.use(notFound)
+
+app.listen(port, () => {
+  console.log(`Sono in ascolto sulla porta ${port}`);
+})
