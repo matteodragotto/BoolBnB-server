7915c7500b2737dd70991688c7c46bdd2ce072f5
galdoben <galdobenedetta@gmail.com>
2025-02-24 12:03:22 +0100

    fix: modifiche-rotte

--- controllers/b&bController.js
+++ controllers/b&bController.js
@@ -1,14 +1,13 @@
 const connection = require('../data/db')
 const path = require('path')
-const fs = require('fs');
 
 
 const index = (req, res) => {
-  const sql = `SELECT real_estate.*, 
+  const sql = `SELECT apartaments.*, 
   GROUP_CONCAT(images.url ORDER BY images.id) AS image_urls
-  FROM real_estate
-  LEFT JOIN images ON images.real_estate_id = real_estate.id
-  GROUP BY real_estate.id;`
+  FROM apartaments
+  LEFT JOIN images ON images.apartaments_id = apartaments.id
+  GROUP BY apartaments.id;`
 
   connection.query(sql, (err, results) => {
     if (err) return res.status(500).json({ error: err })
@@ -30,21 +29,21 @@ const index = (req, res) => {
 const show = (req, res) => {
   const id = req.params.id;
 
-  const sql = `SELECT I.*, ROUND(AVG(R.voto)) AS media_voti
-  FROM real_estate I
-  LEFT JOIN reviews R ON I.id = R.real_estate_id
-  WHERE I.id = ?
-  GROUP BY I.id`
+  const sql = `SELECT A.*, ROUND(AVG(R.voto)) AS media_voti
+  FROM apartaments A
+  LEFT JOIN reviews R ON A.id = R.apartaments_id
+  WHERE A.id = ?
+  GROUP BY A.id`
 
-  const sqlImage = `SELECT images.url FROM images WHERE real_estate_id = ?`
+  const sqlImage = `SELECT images.url FROM images WHERE apartaments_id = ?`
 
-  const sqlReviews = `SELECT * FROM reviews WHERE real_estate_id = ?`
+  const sqlReviews = `SELECT * FROM reviews WHERE apartaments_id = ?`
 
   connection.query(sql, [id], (err, results) => {
     if (err) return res.status(500).json({ error: err.message })
     if (results.length === 0) return res.status(404).json({ error: 'Immobile non trovato' })
 
-    const real_estate = results[0];
+    const apartaments = results[0];
 
     connection.query(sqlImage, [id], (err, resultsImg) => {
       if (err) return res.status(500).json({ error: err.message })
@@ -59,9 +58,9 @@ const show = (req, res) => {
 
       connection.query(sqlReviews, [id], (err, resultsReviews) => {
         if (err) return res.status(500).json({ error: err.message })
-        real_estate.reviews = resultsReviews.length > 0 ? resultsReviews : [];
+        apartaments.reviews = resultsReviews.length > 0 ? resultsReviews : [];
         res.json({
-          ...real_estate,
+          ...apartaments,
           image_urls: correctedImage
         })
       })
@@ -78,31 +77,31 @@ const store = (req, res) => {
   }
 
   const sql = `
-    INSERT INTO real_estate 
-    (titolo, descrizione, numero_stanze, numero_letti, numero_bagni, metri_quadri, indirizzo_completo, email, tipologia, luogo, prezzo_notte, proprietary_users_id)
+    INSERT INTO apartaments 
+    (titolo, descrizione, numero_stanze, numero_letti, numero_bagni, metri_quadri, indirizzo_completo, email, tipologia, luogo, prezzo_notte, users_id)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
   `;
 
-  connection.query(sql, [titolo, descrizione, numero_stanze, numero_letti, numero_bagni, metri_quadri, indirizzo_completo, email, tipologia, luogo, prezzo_notte, proprietary_users_id], (err, results) => {
+  connection.query(sql, [titolo, descrizione, numero_stanze, numero_letti, numero_bagni, metri_quadri, indirizzo_completo, email, tipologia, luogo, prezzo_notte, users_id], (err, results) => {
     if (err) return res.status(500).send({ error: err.message });
 
-    const real_estate_id = results.insertId;
-    res.status(201).send({ message: 'Immobile creato', real_estate_id });
+    const apartaments_id = results.insertId;
+    res.status(201).send({ message: 'Immobile creato', apartaments_id });
   })
 }
 
 const storeImages = (req, res) => {
 
-  const { tipologia, real_estate_id } = req.body
+  const { tipologia, apartaments_id } = req.body
   const imageName = req.file.filename;
 
-  if (!imageName || !real_estate_id) {
-    return res.status(400).json({ error: 'Immagine o real_estate_id mancanti' });
+  if (!imageName || !apartaments_id) {
+    return res.status(400).json({ error: 'Immagine o apartaments_id mancanti' });
   }
 
-  const sql = 'INSERT INTO images (url, tipologia, real_estate_id) VALUES (?, ?, ?)'
+  const sql = 'INSERT INTO images (url, tipologia, apartaments_id) VALUES (?, ?, ?)'
 
-  connection.query(sql, [imageName, tipologia, real_estate_id], (err, results) => {
+  connection.query(sql, [imageName, tipologia, apartaments_id], (err, results) => {
     if (err) return res.status(500).json({ error: err })
 
     res.json({
@@ -111,56 +110,20 @@ const storeImages = (req, res) => {
   })
 }
 
-const storeInterestedUser = (req, res) => {
 
-  const { nome, cognome, data_di_nascita, numero_telefono, immagine_profilo } = req.body
-
-  if (!nome || !cognome || !data_di_nascita || !numero_telefono) {
-    return res.status(400).json({ error: "Tutti i campi sono necessari" });
-  }
-
-  const sql = 'INSERT INTO interested_users (nome, cognome, data_di_nascita, numero_telefono, immagine_profilo) VALUES (?, ?, ?, ?, ?)'
-
-  connection.query(sql, [nome, cognome, data_di_nascita, numero_telefono, immagine_profilo], (err, results) => {
-    if (err) return res.status(500).json({ error: err })
-
-    res.json({
-      message: 'Utente interessato registrato con successo'
-    });
-  })
-}
-
-const storeProprietaryUser = (req, res) => {
-
-  const { nome, cognome, data_di_nascita, numero_telefono, lingue_parlate, immagine_profilo } = req.body
-
-  if (!nome || !cognome || !data_di_nascita || !numero_telefono || !lingue_parlate) {
-    return res.status(400).json({ error: "Tutti i campi sono necessari" });
-  }
-
-  const sql = 'INSERT INTO proprietary_users (nome, cognome, data_di_nascita, numero_telefono, lingue_parlate, immagine_profilo) VALUES (?, ?, ?, ?, ?, ?)'
-
-  connection.query(sql, [nome, cognome, data_di_nascita, numero_telefono, lingue_parlate, immagine_profilo], (err, results) => {
-    if (err) return res.status(500).json({ error: err })
-
-    res.json({
-      message: 'Utente interessato registrato con successo'
-    });
-  })
-}
 
 const storeReviews = (req, res) => {
   const id = req.params.id
 
-  const { voto, descrizione, interested_users_id } = req.body;
+  const { voto, descrizione, users_id } = req.body;
 
-  if (!voto || !descrizione || !interested_users_id) {
+  if (!voto || !descrizione || !users_id) {
     return res.status(400).json({ error: 'Tutti i campi sono necessari' });
   }
 
-  const sql = 'INSERT INTO reviews (voto, descrizione, real_estate_id, interested_users_id) VALUES (?, ?, ?, ?)'
+  const sql = 'INSERT INTO reviews (voto, descrizione, apartaments_id, users_id) VALUES (?, ?, ?, ?)'
 
-  connection.query(sql, [voto, descrizione, id, interested_users_id], (err, results) => {
+  connection.query(sql, [voto, descrizione, id, users_id], (err, results) => {
     if (err) return res.status(500).json({ error: err })
 
     res.json({
@@ -170,38 +133,12 @@ const storeReviews = (req, res) => {
 
 }
 
-const update = (req, res) => {
-  const id = req.params.id
 
-  const { titolo, descrizione, numero_stanze, numero_letti, numero_bagni, metri_quadri, indirizzo_completo, email, tipologia, luogo, prezzo_notte } = req.body;
-
-  const sql = `UPDATE real_estate 
-  SET titolo = ?, 
-  descrizione = ?, 
-  numero_stanze = ?, 
-  numero_letti = ?, 
-  numero_bagni = ?, 
-  metri_quadri = ?, 
-  indirizzo_completo = ?, 
-  email = ?, 
-  tipologia = ?, 
-  luogo = ?, 
-  prezzo_notte = ? 
-  WHERE real_estate.id = ?
-  `
-
-  connection.query(sql, [titolo, descrizione, numero_stanze, numero_letti, numero_bagni, metri_quadri, indirizzo_completo, email, tipologia, luogo, prezzo_notte, id], (err, results) => {
-    if (err) return res.status(500).json({ error: err })
-
-    res.json({ message: `Le informazioni dell'immobile sono state modificate` })
-  })
-
-}
 
 const modify = (req, res) => {
   const id = req.params.id
 
-  const sql = `UPDATE real_estate SET mi_piace = mi_piace + 1 WHERE real_estate.id = ?`
+  const sql = `UPDATE apartaments SET mi_piace = mi_piace + 1 WHERE apartaments.id = ?`
 
   connection.query(sql, [id], (err, results) => {
     if (err) return res.status(500).json({ error: err })
@@ -210,57 +147,7 @@ const modify = (req, res) => {
   })
 }
 
-const destroy = (req, res) => {
-  const id = req.params.id;
 
-  const selectSql = 'SELECT * FROM images WHERE images.real_estate_id = ?';
-
-  const sqlDelete = `
-    DELETE real_estate, images 
-    FROM real_estate 
-    JOIN images ON real_estate.id = images.real_estate_id
-    WHERE real_estate.id = ?`;
-
-  connection.query(selectSql, [id], (err, results) => {
-    if (err) {
-      return res.status(500).json({ error: 'Errore nel recuperare le immagini: ' + err });
-    }
-
-    const imageNames = results.map(image => image.url);
-
-    const imagePaths = imageNames.map(imageName =>
-      path.join(__dirname, '../public/img/immobili', imageName)
-    );
-
-    const deleteImages = imagePaths.map(imagePath => {
-      return new Promise((resolve, reject) => {
-        fs.unlink(imagePath, (err) => {
-          if (err) {
-            console.error('Errore durante la cancellazione del file:', err);
-            reject(err);
-          } else {
-            console.log('File eliminato con successo:', imagePath);
-            resolve();
-          }
-        });
-      });
-    });
-
-    Promise.all(deleteImages)
-      .then(() => {
-        connection.query(sqlDelete, [id], (err) => {
-          if (err) {
-            return res.status(500).json({ error: 'Errore nella cancellazione del record: ' + err });
-          }
-
-          res.json({ message: 'Immobile e immagini eliminati con successo' });
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: 'Errore nella cancellazione delle immagini: ' + err });
-      });
-  });
-};
 
 
 module.exports = {
@@ -269,9 +156,5 @@ module.exports = {
   store,
   storeImages,
   storeReviews,
-  storeInterestedUser,
-  storeProprietaryUser,
-  update,
-  modify,
-  destroy
+  modify
 }
